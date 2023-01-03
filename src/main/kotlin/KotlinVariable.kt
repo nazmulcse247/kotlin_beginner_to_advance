30d439af57bf914ffeb9b42ec81d95fb4f54d54c
Nazmul Hoque <30749074+nazmulcse247@users.noreply.github.com>
2023-01-03 16:54:16 +0600

    hello worl and variable

---
+++ src/main/kotlin/KotlinVariable.kt
@@ -0,0 +1,20 @@
+
+fun main(){
+
+    val name = "Nazmul Hoque"
+    val age = 23
+    val height = 5.6
+
+    print("Name:"+name + " " +"Age:"+age+" "+ "Height:"+height)
+
+
+    //Define type
+
+    var number : Int = 0
+    number = 100
+    var userName : String = "Nazmul"
+    userName = "Nazmul Hoque"
+
+    println(number)
+    println(userName)
+}
