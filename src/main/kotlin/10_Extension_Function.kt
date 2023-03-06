cdeb8dcfff6bd0a7ee80f3ae08cb8b2084ccbc8d
Nazmul Hoque <30749074+nazmulcse247@users.noreply.github.com>
2023-03-06 16:30:50 +0600

    add: extension funtion

---
+++ src/main/kotlin/10_Extension_Function.kt
@@ -0,0 +1,25 @@
+
+/*
+*  Kotlin extension function provides a facility to "add" methods to class without inheriting a class or using any type of design pattern.
+*  The created extension functions are used as a regular function inside that class.
+*  The extension function is declared with a prefix receiver type with method name.
+* */
+
+fun main(args : Array<String>){
+    val s1 = "Hello"
+    val s2 = "World"
+
+    val num1  = 10
+    val num2  = 20
+
+    println(s1.add(s1, s2))
+    println(num1.sum(num1,num2))
+}
+
+fun String.add(s1 : String , s2 : String) : String {
+    return s1 + s2
+}
+
+fun Int.sum(num1 : Int, num2 : Int) : Int{
+    return num1 + num2
+}
--- src/main/kotlin/9_inlineFunction.kt
+++ src/main/kotlin/9_inlineFunction.kt
@@ -1,15 +1,23 @@
-
 /*
 An Inline function is a kind of function that is declared with the keyword "inline" just before the function declaration.
 Once a function is declared inline, the compiler does not allocate any memory for this function, instead the compiler copies the piece of code virtually at the calling place at runtime
 */
-fun main(args : Array<String>){
+
+//inline
+//crossInline
+//noinline
+fun main(args: Array<String>) {
 
     sum(
         20,
         10,
         sum = {
             println("sum : $it")
+            /*if (it == 30){
+                return
+            }
+            println("Run this code block depend on crossInline")*/
+
         },
         sub = {
             println("sub : $it")
@@ -18,7 +26,12 @@ fun main(args : Array<String>){
     )
 }
 
-inline fun sum(firstNum: Int, secondNum: Int, sum: (Int) -> Unit, sub: (Int) -> Unit) {
+inline fun sum(
+    firstNum: Int,
+    secondNum: Int,
+    crossinline sum: (Int) -> Unit,
+    noinline sub: (Int) -> Unit
+) {
 
     val sumTotal = firstNum + secondNum
     val subTotal = firstNum - secondNum
