d6cb59eff597c264289684c3584a7a381279d4e8
Nazmul Hoque <30749074+nazmulcse247@users.noreply.github.com>
2026-05-12 11:52:36 +0600

    Update 16_Abstraction.kt

--- src/main/kotlin/16_Abstraction.kt
+++ src/main/kotlin/16_Abstraction.kt
@@ -1,30 +1,55 @@
-fun main(args : Array<String>){
+Abstraction means: Hide implementation details and show only essential behavior.
 
-    val islamiBanks = IslamiBanks()
-    println(islamiBanks.getInterest())
+| Rule                                 | Meaning              |
+| ------------------------------------ | -------------------- |
+| Abstract class cannot create object  | Incomplete blueprint |
+| Abstract function has no body        | Child must implement |
+| Child must override abstract methods | Required             |
 
+fun main() {
+
+    val emp1: Employee =
+        FullTimeEmployee("Nazmul",50000.0,10000.0)
+
+    val emp2: Employee =
+        PartTimeEmployee("Rahim",100,200.0)
+
+    emp1.showName()
+    println(emp1.calculateSalary())
+
+    emp2.showName()
+    println(emp2.calculateSalary())
 }
+    
+abstract class Employee(
+    val name: String
+) {
 
-abstract class CentralBank() {
-    abstract fun getInterest() : Int
-}
+    abstract fun calculateSalary(): Double
 
-class IslamiBanks : CentralBank() {
-    override fun getInterest(): Int {
-        return 10
+    fun showName() {
+        println("Employee Name: $name")
     }
-
 }
 
-class PrimeBanks : CentralBank() {
-    override fun getInterest(): Int {
-        return 15
+class FullTimeEmployee(
+    name: String,
+    private val salary: Double,
+    private val bonus: Double
+) : Employee(name) {
+
+    override fun calculateSalary(): Double {
+        return salary + bonus
     }
-
 }
 
-class AsiaBank : CentralBank() {
-    override fun getInterest(): Int {
-        return 20
+class PartTimeEmployee(
+    name: String,
+    private val hours: Int,
+    private val rate: Double
+) : Employee(name) {
+
+    override fun calculateSalary(): Double {
+        return hours * rate
     }
 }
